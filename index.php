61a179ac6012e8eb1555e585a6c4baeded9e9f83
Abraham Guo <abraham@csm.cc>
2018-07-23 09:17:32 -0500

    Cursor fix in progres

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once("inc/init.inc.php");
-$body_cls = 'fullscreen';
+$body_cls = 'fullscreen player';
 $data_page = 'welcome';
 include("inc/head.inc.php");
 
@@ -28,6 +28,30 @@ include("inc/head.inc.php");
 			</a>
 		" : ""?>
 	</div>
+</section>
+
+<section data-view="player">
+	<video id="player" data-index="0"></video>
+	<ul id="playlist">
+	<?php
+
+	$n = 1;
+	foreach ($playlist as $video){
+		if (file_exists($video_dir.$video['id'].'.mp4')) {
+			echo '<li id="v'.$video['id'].'" data-id="'.$video['id'].'" data-hotkey="'.$video['hotkey'].'" data-video="'.$video_url.$video['id'].'.mp4"><span><img src="img/chevron-right.png"></span>'.$video['title'];
+			if ($video['hotkey'] != '') echo '<div>Hot key: '.$video['hotkey'].'</div>';
+			if ($n == count($playlist)) {
+				echo '<div style="margin-top:20px; text-align:right">Esc to exit</div>';
+			}
+			echo '</li>';
+		}
+		$n++;
+	}
+
+	?>
+	</ul>
+	<div id="video-title"></div>
+	<div id="require-setup">This kiosk requires set-up. Please hit <em>ESC</em> to return to main menu.</div>
 </section>
 
 <?php
